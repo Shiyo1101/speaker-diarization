94b73a8d451cf2dca81d54fc863c7dc91bdff667
Shiyo1101 <yoshiki1011abc@gmail.com>
2025-07-06 06:02:29 +0000

    🩹 軽微修正

--- src/app/entrypoint.sh
+++ src/app/entrypoint.sh
@@ -2,5 +2,6 @@
 set -e
 
 echo '🚀 FastAPI server is running at http://localhost:8000'
+echo '📝 Swagger UI is available at http://localhost:8000/docs'
 
 exec /opt/venv/bin/uvicorn src.app.main:app --host 0.0.0.0 --port 80 --reload --reload-dir src/app
